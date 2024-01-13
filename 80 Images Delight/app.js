a871eb94b721a3c2a9c5309036d07104ca7d12bc
MumuXNYK <mumukshnayak2904@hotmail.com>
2024-01-13 12:48:35 +0530

    Animated Image Section

---
+++ 80 Images Delight/app.js
@@ -0,0 +1,22 @@
+let btns = document.querySelectorAll('.btn')
+let banner = document.getElementById('banner')
+
+btns.forEach((btn,i) =>{
+    btn.addEventListener('click',()=>{
+        banner.src = `asset/${i}.jpg`
+        abimation()
+        btn.classList.add('active')
+    })
+})
+
+function abimation() {
+    banner.classList.add('zoom')
+
+    setTimeout(() => {
+        banner.classList.remove('zoom')
+    }, 1000);
+
+btns.forEach(b =>{
+    b.classList.remove('active')
+})
+}
